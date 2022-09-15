c1cf2dad5f64c538b9001ddbb9d219014de8e78b
Pidkova <parmelion12@gmail.com>
2022-09-15 13:50:35 +0200

    Added the link to GitHub-repository

--- src/App.js
+++ src/App.js
@@ -5,8 +5,19 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
       <Weather defaultCity="Zurich" />
+      <br />
+      <div className="author">
+        <span className="me-2">
+          <a
+            className="code-link"
+            href="https://github.com/Pidkova/react_weather_app"
+          >
+            Coded with ♥
+          </a>
+        </span>
+        by Tania Pidkova
+      </div>
     </div>
   );
 }
